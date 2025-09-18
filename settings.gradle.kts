2ae460c27c8251c14563fc3048e091e4eedd87fe
alpha3002025 <alpha300uk@gmail.com>
2025-09-18 15:57:58 +0900

    submodule update

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ rootProject.name = "dailyfeed-timeline-svc"
 include(
     "dailyfeed-code",
     "dailyfeed-pagination-support",
+    "dailyfeed-redis-support",
     "dailyfeed-feign",
     "dailyfeed-timeline",
 )
