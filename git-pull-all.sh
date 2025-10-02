9e6b462b564bb7ca4d440f1029e8fff3cc69a74a
alpha3002025 <alpha300uk@gmail.com>
2025-10-02 16:17:08 +0900

    submodule update

---
+++ git-pull-all.sh
@@ -0,0 +1,42 @@
+echo "dailyfeed-code"
+cd dailyfeed-code
+git pull origin main
+cd ..
+echo ""
+
+echo "dailyfeed-feign"
+cd dailyfeed-feign
+git pull origin main
+cd ..
+echo ""
+
+
+echo "dailyfeed-infrastructure"
+cd dailyfeed-infrastructure
+git pull origin main
+cd ..
+echo ""
+
+
+echo "dailyfeed-pagination-support"
+cd dailyfeed-pagination-support
+git pull origin main
+cd ..
+echo ""
+
+
+echo "dailyfeed-redis-support"
+cd dailyfeed-redis-support
+git pull origin main
+cd ..
+echo ""
+
+
+echo "dailyfeed-timeline"
+cd dailyfeed-timeline
+git pull origin main
+cd ..
+echo ""
+
+
+
